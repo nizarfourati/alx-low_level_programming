5d1a8ed1eae98613c8fe94c1a4a802032f4340cd
nizarfourati <nizarfourati.13@gmail.com>
2023-07-13 10:48:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <studio>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main (void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
