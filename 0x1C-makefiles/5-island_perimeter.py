210f902674cfa3690840f0a9270c5f6729f78ff5
nizar fourati <nizarfourati.13@gmail.com>
2024-03-27 19:32:35 -0700

    lol

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,45 @@
 #!/usr/bin/python3
-""" Island perimeter """
+""" doc doc """
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    edge = 0
-    size = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                size += 1
-                if j > 1 and grid[i][j - 1] == 1:
-                    edge += 1
-                if i > 0 and grid[i - 1][j] == 1:
-                    edge += 1
+    """doc doc"""
+    counter = 0
+    for row_i in range(len(grid)):
+        for col_i in range(len(grid[row_i])):
+            if grid[row_i][col_i] == 1:
+                counter += count_water(grid, row_i, col_i)
+    return counter
 
-    result = size * 4 - edge * 2
-    return result
+
+def count_water(grid, row_i, col_i):
+    """doc doc"""
+    counter = 0
+    value = grid[row_i][col_i]
+    max_i_col = len(grid[row_i]) - 1
+    max_i_row = len(grid) - 1
+    if col_i > 0:
+        left = grid[row_i][col_i - 1]
+        if left == 0:
+            counter += 1
+    else:
+        counter += 1
+    if col_i < max_i_col:
+        right = grid[row_i][col_i + 1]
+        if right == 0:
+            counter += 1
+    else:
+        counter += 1
+    if row_i > 0:
+        up = grid[row_i - 1][col_i]
+        if up == 0:
+            counter += 1
+    else:
+        counter += 1
+    if row_i < max_i_row:
+        down = grid[row_i + 1][col_i]
+        if down == 0:
+            counter += 1
+    else:
+        counter += 1
+    return counter
